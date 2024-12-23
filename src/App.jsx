d7a42e6d6995efc65d4c7e8aac4a3b6038916ce9
ArunAkilan <arun@cloudgarage.io>
2024-12-23 10:38:12 +0530

    community topic added

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>I am Arunkumar Selvaraj</h1>
+	  <h1> Welcome to DevOps Community </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
